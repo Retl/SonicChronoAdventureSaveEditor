8f5a6acdff8dd584888f6f931f40255b88f55cc4
HeliumSqueeze LLC <cmoore@heliumsqueeze.com>
2019-06-01 20:46:42 -0500

    Initial Commit. Seems to partially work for reading smaller values, but does not save at all. Values become invalid around 200+. Try the included sample save.

---
+++ SonicChronoAdventureSaveEdit/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SonicChronoAdventureSaveEdit
+{
+    partial class frmSCASaveEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.opnSaveFile = new System.Windows.Forms.OpenFileDialog();
+            this.saveSaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.grdSaveData = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.colIndex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grdSaveData)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(3, 0);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(104, 0);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // opnSaveFile
+            // 
+            this.opnSaveFile.FileName = "BTSAsave.bin";
+            this.opnSaveFile.FileOk += new System.ComponentModel.CancelEventHandler(this.opnSaveFile_FileOk);
+            // 
+            // saveSaveFile
+            // 
+            this.saveSaveFile.FileName = "BTSAsave.bin";
+            // 
+            // grdSaveData
+            // 
+            this.grdSaveData.AllowUserToAddRows = false;
+            this.grdSaveData.AllowUserToDeleteRows = false;
+            this.grdSaveData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grdSaveData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.grdSaveData.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.grdSaveData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdSaveData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colIndex,
+            this.colValue});
+            this.grdSaveData.Location = new System.Drawing.Point(3, 3);
+            this.grdSaveData.Name = "grdSaveData";
+            this.grdSaveData.Size = new System.Drawing.Size(243, 467);
+            this.grdSaveData.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Location = new System.Drawing.Point(15, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(182, 26);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.grdSaveData);
+            this.panel2.Location = new System.Drawing.Point(12, 44);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(249, 470);
+            this.panel2.TabIndex = 5;
+            // 
+            // colIndex
+            // 
+            this.colIndex.HeaderText = "Index";
+            this.colIndex.Name = "colIndex";
+            this.colIndex.Width = 58;
+            // 
+            // colValue
+            // 
+            this.colValue.HeaderText = "Value";
+            this.colValue.Name = "colValue";
+            this.colValue.Width = 59;
+            // 
+            // frmSCASaveEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(273, 526);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmSCASaveEdit";
+            this.Text = "Sonic Chrono Adventure Save Edit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdSaveData)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.OpenFileDialog opnSaveFile;
+        private System.Windows.Forms.SaveFileDialog saveSaveFile;
+        private System.Windows.Forms.DataGridView grdSaveData;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colIndex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colValue;
+    }
+}
+
---
+++ SonicChronoAdventureSaveEdit/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SonicChronoAdventureSaveEdit
+{
+    public partial class frmSCASaveEdit : Form
+    {
+        private int[] arrInts = new int[256];
+        List<int> arrData = new List<int>(256);
+        List<int> arrDataHeader = new List<int>(8);
+        List<byte> arrDataFooter = new List<byte>(8);
+        public frmSCASaveEdit()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            grdSaveData.ColumnCount = 2;
+            grdSaveData.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            grdSaveData.Columns[0].Name = "Index";
+            grdSaveData.Columns[1].Name = "Value";
+            MapDataToDataGrid(new List<int>(arrInts), grdSaveData);
+        }
+
+        private void MapDataToListView(List<int> data, ListView lsv) {
+            lsv.Items.Clear();
+            for (int i = 0; i < data.Count; i++) {
+                lsv.Items.Add(new ListViewItem());
+                lsv.Items[i].Text = data[i].ToString();
+            }
+        }
+
+        private void MapDataToDataGrid(List<int> data, DataGridView grd)
+        {
+            grd.Rows.Clear();
+            for (int i = 0; i < data.Count; i++)
+            {
+                string rowName;
+
+                switch (i)
+                {
+                    case 0:
+                        rowName = "StoryVal";
+                        break;
+                    case 1:
+                        rowName = "SubStory1";
+                        break;
+                    case 2:
+                        rowName = "SubStory2";
+                        break;
+                    case 3:
+                        rowName = "SubStory3";
+                        break;
+                    case 12:
+                        rowName = "jetLVL";
+                        break;
+                    case 13:
+                        rowName = "sowLVL";
+                        break;
+                    case 14:
+                        rowName = "modLVL";
+                        break;
+                    case 15:
+                        rowName = "burnLVL";
+                        break;
+                    case 16:
+                        rowName = "StageFrame";
+                        break;
+                    case 80:
+                        rowName = "JetEXP";
+                        break;
+                    case 81:
+                        rowName = "SowEXP";
+                        break;
+                    case 82:
+                        rowName = "ModEXP";
+                        break;
+                    case 83:
+                        rowName = "BurnEXP";
+                        break;
+                    case 94:
+                        rowName = "Emeralds";
+                        break;
+                    case 153:
+                        rowName = "Rings";
+                        break;
+                    default:
+                         rowName = i.ToString();
+                        break;
+                }
+                    
+
+                string[] rowAsTextArray = { rowName, data[i].ToString()};
+                grd.Rows.Add(rowAsTextArray);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            opnSaveFile.ShowDialog();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveSaveFile.ShowDialog();
+        }
+
+        private void opnSaveFile_FileOk(object sender, CancelEventArgs e)
+        {
+            arrData.Clear();
+            arrDataHeader.Clear();
+            arrDataFooter.Clear();
+            String filePath = opnSaveFile.FileName;
+
+            //Read the contents of the file into a stream
+
+            using (Stream fileStream = opnSaveFile.OpenFile())
+            {
+                /*using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    String fileContent = reader.ReadToEnd();
+                    Console.WriteLine(fileContent);
+                }*/
+
+                using (BinaryReader2 r = new BinaryReader2(fileStream))
+                {
+                    int readCount = 0;
+
+                    if (r.BaseStream.Length > 3000) {
+                        Console.WriteLine("File seems way too large.");
+                        arrData.Clear();
+                        arrData.Add(-9999);
+                        MapDataToDataGrid(arrData, grdSaveData);
+                        return;
+                    }
+
+                    while (r.BaseStream.Position != r.BaseStream.Length)
+                    {
+                        try
+                        {
+                            if (readCount < 1) // Header is always at the front.
+                            {
+                                arrDataHeader.Add(r.ReadInt32());
+                            }
+                            else if (readCount >= 257) // Must count the header. We're expecting 256 ints only.
+                            {
+                                arrDataFooter.Add(r.ReadByte());
+                            }
+                            else if (readCount >= 300) // Very basic wrong-file protection.
+                            {
+                                arrData.Clear();
+                                arrData.Add(-9999);
+                                break;
+                            }
+                            else
+                            {
+                                arrData.Add(r.ReadInt32());
+                            }
+                            readCount++;
+                        }
+                        catch (System.ArgumentException)
+                        {
+                            Console.WriteLine("Just as a heads up, you had that ARgument Exception error again.");
+                        }
+                        catch (System.IO.EndOfStreamException)
+                        {
+                            Console.WriteLine("Just as a heads up, you had that End of Steam Exception this time...");
+                        }
+                    }
+                    MapDataToDataGrid(arrData, grdSaveData);
+                }
+            }
+        }
+
+        // A Big-Endian approach to reading these values.
+        // Taken directly from: https://stackoverflow.com/a/8621184
+        class BinaryReader2 : BinaryReader
+        {
+            public BinaryReader2(System.IO.Stream stream) : base(stream) { }
+
+            public override int ReadInt32()
+            {
+                var data = base.ReadBytes(4);
+                Array.Reverse(data);
+                return BitConverter.ToInt32(data, 0);
+            }
+
+            public Int16 ReadInt16()
+            {
+                var data = base.ReadBytes(2);
+                Array.Reverse(data);
+                return BitConverter.ToInt16(data, 0);
+            }
+
+            public Int64 ReadInt64()
+            {
+                var data = base.ReadBytes(8);
+                Array.Reverse(data);
+                return BitConverter.ToInt64(data, 0);
+            }
+
+            public UInt32 ReadUInt32()
+            {
+                var data = base.ReadBytes(4);
+                Array.Reverse(data);
+                return BitConverter.ToUInt32(data, 0);
+            }
+
+        }
+    }
+}
